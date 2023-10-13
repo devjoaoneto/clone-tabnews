a1d9e9068fe467c2aa890fd714d88443c715946f
João Neto <100517304+devjoaoneto@users.noreply.github.com>
2023-10-13 19:32:13 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1> Louise se você está lendo essa mensagem saiba que te amo muito, quando você ver essa mensagem da um sorisso e tira uma selfie, fico no aguardo.</h1>
+}
+
+export default Home;
